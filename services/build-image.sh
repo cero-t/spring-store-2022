758b558f94c352f4cd0d458e67b264c4dbcd9656
Shin Tanimoto <shin@tanimo.to>
2022-12-31 07:33:02 +0900

    Make spring-boot:image-build available.

---
+++ services/build-image.sh
@@ -0,0 +1,1 @@
+./mvnw clean package spring-boot:build-image -am -pl item/item-service,stock/stock-service,cart/cart-service,payment/payment-service,order/order-service,delivery/delivery-service,bff,messaging
