0c765b139df8f481311ca7f4b00467bd1ad87fbb
Shin Tanimoto <cero-t@users.noreply.github.com>
2023-01-10 10:36:05 +0900

    Enable observation (for micrometer tracing)

--- services/messaging/src/main/java/ninja/cero/store/messaging/MessagingConfig.java
+++ services/messaging/src/main/java/ninja/cero/store/messaging/MessagingConfig.java
@@ -1,14 +1,18 @@
 package ninja.cero.store.messaging;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class MessagingConfig {
+public class MessagingConfig implements BeanPostProcessor {
     @Bean
     WebClient webClient(WebClient.Builder builder) {
         return builder.build();
@@ -22,5 +26,17 @@ public class MessagingConfig {
     @Bean
     Queue queue() {
         return new Queue("messaging");
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+        if (bean instanceof RabbitTemplate template) {
+            template.setObservationEnabled(true);
+        }
+        if (bean instanceof SimpleRabbitListenerContainerFactory factory) {
+            factory.setObservationEnabled(true);
+        }
+
+        return bean;
     }
 }
